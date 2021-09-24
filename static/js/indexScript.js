7fe66188b0adf983677ffff1c05b7dca83b23458
kkj <rudwnd33@naver.com>
2021-09-25 04:12:44 +0900

    index 페이지 js 파일

--- static/js/indexScript.js
+++ static/js/indexScript.js
@@ -5,21 +5,39 @@ function slide() {
 
         // $('.slider-div').slick({
         $('.slider-div').not('.slick-initialized').slick({
-            slide: 'div',		//슬라이드 되어야 할 태그 ex) div, li
+            slide: 'li',		//슬라이드 되어야 할 태그 ex) div, li
             infinite: true, 	//무한 반복 옵션
             slidesToShow: 3,		// 한 화면에 보여질 컨텐츠 개수
             slidesToScroll: 1,		//스크롤 한번에 움직일 컨텐츠 개수
             speed: 100,	 // 다음 버튼 누르고 다음 화면 뜨는데까지 걸리는 시간(ms)
-            arrows: true, 		// 옆으로 이동하는 화살표 표시 여부
             dots: true, 		// 스크롤바 아래 점으로 페이지네이션 여부
             autoplay: true,			// 자동 스크롤 사용 여부
-            autoplaySpeed: 10000, 		// 자동 스크롤 시 다음으로 넘어가는데 걸리는 시간 (ms)
-            pauseOnHover: true,		// 슬라이드 이동	시 마우스 호버하면 슬라이더 멈추게 설정
+            autoplaySpeed: 5000, 		// 자동 스크롤 시 다음으로 넘어가는데 걸리는 시간 (ms)
+            pauseOnHover: true,		// 슬라이드 이동 시 마우스 호버하면 슬라이더 멈추게 설정
             vertical: false,		// 세로 방향 슬라이드 옵션
-            prevArrow: $('#btn_prev'),		// 이전 화살표 모양 설정
-            nextArrow: $('#btn_next'),		// 다음 화살표 모양 설정
+            // arrows: true, 		// 옆으로 이동하는 화살표 표시 여부
+            // prevArrow: $('#btn_prev'),		// 이전 화살표 모양 설정
+            // nextArrow: $('#btn_next'),		// 다음 화살표 모양 설정
             dotsClass: "slick-dots", 	//아래 나오는 페이지네이션(점) css class 지정
             draggable: true, 	//드래그 가능 여부
+
+            responsive: [ // 반응형 웹 구현 옵션
+                {
+                    breakpoint: 1500, //화면 사이즈 1500px 보다 작을 시
+                    settings: {
+                        //위에 옵션이 디폴트 , 여기에 추가하면 그걸로 변경
+                        slidesToShow: 2
+                    }
+                },
+                {
+                    breakpoint: 800, //화면 사이즈 800px 보다 작을 시
+                    settings: {
+                        //위에 옵션이 디폴트 , 여기에 추가하면 그걸로 변경
+                        slidesToShow: 1
+                    }
+                }
+            ]
+
         });
     })
 }
@@ -34,19 +52,32 @@ function geoInfo() {
             url: "/near",
             data: {lat_give: lat, lng_give: lng},
             success: function (response) {
+                $('#nearCard').empty();
                 nearList = response['nearList'];
                 for (let i = 0; i < nearList.length; i++) {
                     let title = nearList[i]['title'];
                     let address = nearList[i]['addr1'];
                     let image = nearList[i]['firstimage'];
+                    let distance = nearList[i]['dist'];
 
-                    let temp_html = `<div class="card h-150">
-                                                        <img class="card-img-top" src="${image}" alt="내 위치 근처 여행지">
-                                                        <div class="card-body">
-                                                            <h5 class="card-title">${title}</h5>
-                                                            <p class="card-text">${address}</p>
-                                                        </div>
-                                                 </div>`
+                    let temp_html = `<li style="margin: 0 10px; height: 300px;">
+                                        <a href="" class="card">
+                                            <img src="${image}" class="card__image" alt="내 위치 근처 여행지 사진"/>
+                                            <div class="card__overlay">
+                                                <div class="card__header">
+                                                    <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
+                                                        <path/>
+                                                    </svg>
+                                                    <img class="card__thumb" src="${image}" alt="썸네일"/>
+                                                    <div class="card__header-text">
+                                                        <h3 class="card__title">${title}</h3>
+                                                        <span class="card__status">${distance}m</span>
+                                                    </div>
+                                                </div>
+                                                <p class="card__description">${address}</p>
+                                            </div>
+                                        </a>
+                                    </li>`
                     $('#nearCard').append(temp_html);
                     slide();
                 }
