39331f882cae1513339e91f0e143e9f33a9762f1
kkj <rudwnd33@naver.com>
2021-09-26 22:17:28 +0900

    슬라이드 버튼 css 조정 & 여행지 리뷰 슬라이드 생성

--- codeShare/KJ/static/js/indexScript.js
+++ codeShare/KJ/static/js/indexScript.js
@@ -4,8 +4,8 @@ function slide() {
         // Uncaught TypeError: Cannot read property 'add' of null” 오류 -> slick을 여러번 불러와서 발생
         // .not('.slick-initialized')로 하면 오류가 안 난다.
 
-        // $('.slider-div').slick({
-        $('.slider-div').not('.slick-initialized').slick({
+        // $('.slider-li').slick({
+        $('.slider-li').not('.slick-initialized').slick({
             slide: 'li',		//슬라이드 되어야 할 태그 ex) div, li
             infinite: true, 	//무한 반복 옵션
             slidesToShow: 3,		// 한 화면에 보여질 컨텐츠 개수
@@ -59,18 +59,18 @@ function geoInfo() {
                 for (let i = 0; i < nearList.length; i++) {
                     let title = nearList[i]['title'];
                     let address = nearList[i]['addr1'];
-                    let image = nearList[i]['firstimage'];
+                    let file = nearList[i]['firstimage'];
                     let distance = nearList[i]['dist'];
 
                     let temp_html = `<li style="margin: 0 10px; height: 300px;">
                                         <a href="#" class="card">
-                                            <img src="${image}" class="card__image" alt="내 위치 근처 여행지 사진"/>
+                                            <img src="${file}" class="card__image" alt="내 위치 근처 여행지 사진"/>
                                             <div class="card__overlay">
                                                 <div class="card__header">
                                                     <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
                                                         <path/>
                                                     </svg>
-                                                    <img class="card__thumb" src="${image}" alt="썸네일"/>
+                                                    <img class="card__thumb" src="${file}" alt="썸네일"/>
                                                     <div class="card__header-text">
                                                         <h3 class="card__title">${title}</h3>
                                                         <span class="card__status">${distance}m</span>
@@ -95,67 +95,84 @@ function geoInfo() {
     navigator.geolocation.getCurrentPosition(onGeoOK, onGeoError);
 }
 
-// 파일 업로더 js
+function slide2() {
+    $(function () {
+        // Uncaught TypeError: Cannot read property 'add' of null” 오류 -> slick을 여러번 불러와서 발생
+        // .not('.slick-initialized')로 하면 오류가 안 난다.
 
-function readURL(input) {
-    if (input.files && input.files[0]) {
+        // $('.slider-li2').slick({
+        $('.slider-li2').not('.slick-initialized').slick({
+            slide: 'li',		//슬라이드 되어야 할 태그 ex) div, li
+            infinite: true, 	//무한 반복 옵션
+            slidesToShow: 3,		// 한 화면에 보여질 컨텐츠 개수
+            slidesToScroll: 1,		//스크롤 한번에 움직일 컨텐츠 개수
+            speed: 100,	 // 다음 버튼 누르고 다음 화면 뜨는데까지 걸리는 시간(ms)
+            dots: true, 		// 스크롤바 아래 점으로 페이지네이션 여부
+            // autoplay: true,			// 자동 스크롤 사용 여부
+            autoplaySpeed: 5000, 		// 자동 스크롤 시 다음으로 넘어가는데 걸리는 시간 (ms)
+            pauseOnHover: true,		// 슬라이드 이동 시 마우스 호버하면 슬라이더 멈추게 설정
+            vertical: false,		// 세로 방향 슬라이드 옵션
+            arrows: true, 		// 옆으로 이동하는 화살표 표시 여부
+            prevArrow: $('#btn_prev2'),		// 이전 화살표 모양 설정
+            nextArrow: $('#btn_next2'),		// 다음 화살표 모양 설정
+            dotsClass: "slick-dots", 	//아래 나오는 페이지네이션(점) css class 지정
+            draggable: true, 	//드래그 가능 여부
 
-        var reader = new FileReader();
+            responsive: [ // 반응형 웹 구현 옵션
+                {
+                    breakpoint: 1500, //화면 사이즈 1500px 보다 작을 시
+                    settings: {
+                        //위에 옵션이 디폴트 , 여기에 추가하면 그걸로 변경
+                        slidesToShow: 2
+                    }
+                },
+                {
+                    breakpoint: 800, //화면 사이즈 800px 보다 작을 시
+                    settings: {
+                        //위에 옵션이 디폴트 , 여기에 추가하면 그걸로 변경
+                        slidesToShow: 1
+                    }
+                }
+            ]
 
-        reader.onload = function (e) {
-            $('.image-upload-wrap').hide();
-
-            $('.file-upload-image').attr('src', e.target.result);
-            $('.file-upload-content').show();
-
-            $('.image-title').html(input.files[0].name);
-        };
-
-        reader.readAsDataURL(input.files[0]);
-
-    } else {
-        removeUpload();
-    }
+        });
+    })
 }
 
-function removeUpload() {
-    $('.file-upload-input').replaceWith($('.file-upload-input').clone());
-    $('.file-upload-content').hide();
-    $('.image-upload-wrap').show();
-}
-
-$('.image-upload-wrap').bind('dragover', function () {
-    $('.image-upload-wrap').addClass('image-dropping');
-});
-$('.image-upload-wrap').bind('dragleave', function () {
-    $('.image-upload-wrap').removeClass('image-dropping');
-});
-
-
-// 리뷰 작성
-function save() {
-    let title = $('#title').val()
-    let place = $('#place').val()
-    let review = $('#review').val()
-    let file = $('#file')[0].files[0]
-
-    let form_data = new FormData()
-
-    form_data.append("file_give", file)
-    form_data.append("title_give", title)
-    form_data.append("place_give", place)
-    form_data.append("review_give", review)
-
+function showTrips() {
     $.ajax({
-        type: "POST",
+        type: "GET",
         url: "/trips",
-        data: form_data,
-        cache: false,
-        contentType: false,
-        processData: false,
+        data: {},
         success: function (response) {
-            alert(response["msg"])
-            window.location.reload()
+            tripList = response['all_trips'];
+            for (let i = 0; i < tripList.length; i++) {
+                let trip_title = tripList[i]['title'];
+                let trip_place = tripList[i]['place'];
+                let trip_file = tripList[i]['file'];
+                let trip_date = tripList[i]['date'];
+
+                let temp_html = `<li style="margin: 0 10px; height: 300px;">
+                                        <a href="#" class="card">
+                                            <img src="../static/img/${trip_file}" class="card__image" alt="내 위치 근처 여행지 사진"/>
+                                            <div class="card__overlay">
+                                                <div class="card__header">
+                                                    <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
+                                                        <path/>
+                                                    </svg>
+                                                    <img class="card__thumb" src="../static/img/${trip_file}" alt="썸네일"/>
+                                                    <div class="card__header-text">
+                                                        <h3 class="card__title">${trip_title}</h3>
+                                                        <span class="card__status">${trip_date}</span>
+                                                    </div>
+                                                </div>
+                                                <p class="card__description">${trip_place}</p>
+                                            </div>
+                                        </a>
+                                    </li>`
+                $('#tripCard').append(temp_html);
+                slide2();
+            }
         }
-    });
+    })
 }
