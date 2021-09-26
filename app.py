465861dcd62f2de8a6bcdec7cf66849fa8992579
kkj <rudwnd33@naver.com>
2021-09-27 00:01:54 +0900

    최신으로 merge

--- app.py
+++ app.py
@@ -3,21 +3,20 @@ from pymongo import MongoClient
 import requests
 import xmltodict
 import json
+from datetime import datetime
 
 app = Flask(__name__)
 
 client = MongoClient('localhost', 27017)
 db = client.myTrip
 
-
-@app.route('/')
+@app.route('/', methods=['GET'])
 def home():
-
     return render_template('index.html')
+
 
 @app.route('/near', methods=['POST'])
 def near_place():
-
     lat_receive = request.form['lat_give']
     lng_receive = request.form['lng_give']
 
@@ -38,6 +37,49 @@ def near_place():
     nearList = jsonBody['response']['body']['items']['item']
 
     return jsonify({'nearList': nearList})
+
+
+@app.route('/trips/form', methods=['GET'])
+def write():
+    return render_template('write.html')
+
+
+@app.route('/trips', methods=['POST'])
+def write_trip():
+    title_receive = request.form['title_give']
+    place_receive = request.form['place_give']
+    review_receive = request.form['review_give']
+    file = request.files["file_give"]
+
+    print(title_receive, place_receive, review_receive, file)
+
+    today = datetime.now()
+    mytime = today.strftime('%Y-%m-%d-%H-%M-%S')
+    date = today.strftime('%Y.%m.%d')
+
+    filename = f'file-{mytime}'
+    extension = file.filename.split('.')[-1]
+
+    save_to = f'static/img/{filename}.{extension}'
+    file.save(save_to)
+
+    doc = {
+        'title': title_receive,
+        'place': place_receive,
+        'review': review_receive,
+        'file': f'{filename}.{extension}',
+        'date': date
+    }
+
+    db.trips.insert_one(doc)
+    return jsonify({'msg': '작성 완료!'})
+
+@app.route('/trips', methods=['GET'])
+def show_trips():
+    all_trips = list(db.trips.find({}, {'_id': False}))
+
+    return jsonify({'all_trips': all_trips})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
