66419e1b677649fcbf79e55f7266d7784b5c49f0
Woogys <apolontes@hanmail.net>
2021-09-27 16:31:31 +0900

    추가-내가 가본 여행지 추천 기능

--- codeShare/TW/app.py
+++ codeShare/TW/app.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template, jsonify, request
+
 from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.myTrip
+
+from datetime import datetime
+
 import requests
 import xmltodict
 import json
 
 app = Flask(__name__)
-
-client = MongoClient('localhost', 27017)
-db = client.myTrip
 
 
 @app.route('/')
@@ -38,6 +41,38 @@ def near_place():
     print(nearList)
 
     return jsonify({'nearList': nearList})
+
+@app.route('/diary', methods=['GET'])
+def show_diary():
+    diaries = list(db.diary.find({}, {'_id': False}))
+    return jsonify({'all_diary': diaries})
+
+@app.route('/diary', methods=['POST'])
+def save_diary():
+    title_receive = request.form['title_give']
+    content_receive = request.form['content_give']
+
+    file = request.files["file_give"]
+
+    extension = file.filename.split('.')[-1] # -1은 "가장 마지막 .을 선택"의 의미
+
+    today = datetime.now()
+    mytime = today.strftime('%Y-%m-%d-%H-%M-%S')
+
+    filename = f'file={mytime}'
+
+    save_to = f'static/{filename}.{extension}'
+    file.save(save_to)
+
+    doc = {
+        'title': title_receive,
+        'content': content_receive,
+        'file': f'{filename}.{extension}'
+    }
+
+    db.diary.insert_one(doc)
+
+    return jsonify({'msg': '추천 완료!'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
