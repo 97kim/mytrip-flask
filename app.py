c9cc1682a3875c50bd916174546f71c383ae19d3
kkj <rudwnd33@naver.com>
2021-09-30 02:49:29 +0900

    api키 환경 변수 설정

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
+import os
+
 from flask import Flask, render_template, jsonify, request
 from pymongo import MongoClient
 import requests
 import xmltodict
 import json
 from datetime import datetime
+# python-dotenv 라이브러리 설치
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
 client = MongoClient('localhost', 27017)
 db = client.myTrip
 
+# .env 파일 만들어서 외부 노출 방지
+load_dotenv(verbose=True)
+OPEN_API_KEY = os.getenv('OPEN_API_KEY')
 
 @app.route('/', methods=['GET'])
 def home():
@@ -25,17 +32,13 @@ def near_place():
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'
     }
 
-    key = 'tvZKl3gQbwlUbUigX5R%2FmUNfkrT%2FacEC89WdQBGT7XmcGdbuD6n24S98%2B0b4VE0o28TGoIQMjARPyGGpvzpYpw%3D%3D'
-
-    url = f'http://api.visitkorea.or.kr/openapi/service/rest/KorService/locationBasedList?ServiceKey={key}&contentTypeId=12&mapX={lng_receive}&mapY={lat_receive}&radius=4000&listYN=Y&MobileOS=ETC&MobileApp=TourAPI3.0_Guide&arrange=E&numOfRows=10&pageNo=1'
+    url = f'http://api.visitkorea.or.kr/openapi/service/rest/KorService/locationBasedList?ServiceKey={OPEN_API_KEY}&contentTypeId=12&mapX={lng_receive}&mapY={lat_receive}&radius=4000&listYN=Y&MobileOS=ETC&MobileApp=TourAPI3.0_Guide&arrange=E&numOfRows=10&pageNo=1'
 
     r = requests.get(url, headers=headers)
 
     dictionary = xmltodict.parse(r.text)  # xml을 파이썬 객체(딕셔너리)로 변환
     jsonDump = json.dumps(dictionary)  # 파이썬 객체(딕셔너리)를 json 문자열로 변환
     jsonBody = json.loads(jsonDump)  # json 문자열을 파이썬 객체(딕셔너리)로 변환
-
-    print(jsonBody)
 
     nearList = jsonBody['response']['body']['items']['item']
 
