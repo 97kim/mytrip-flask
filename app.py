01988a2dfbf759131c26ac32cce08f986abd732c
Woogys <apolontes@hanmail.net>
2021-10-07 22:46:13 +0900

    삭제 - 즐겨찾기 기능 재설정을 위한 삭제

--- app.py
+++ app.py
@@ -162,29 +162,6 @@ def get_near_detail():
     return render_template('nearDetail.html')
 
 
-@app.route('/near/bookmark', methods=['POST'])
-def bookmark():
-    token_receive = request.cookies.get('mytoken')
-    try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-
-        user_info = db.users.find_one({"username": payload["id"]})
-        content_id_receive = request.form["content_id_give"]
-        action_receive = request.form["action_give"]
-        doc = {
-            "content_id": content_id_receive,
-            "username": user_info["username"],
-        }
-        if action_receive == "like":
-            db.bookmark.insert_one(doc)
-        else:
-            db.bookmark.delete_one(doc)
-
-        return jsonify({"result": "success", 'msg': 'updated'})
-    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-        return redirect(url_for("main"))
-
-
 @app.route('/near/list', methods=['GET'])
 def get_near_list():
     return render_template('nearList.html')
