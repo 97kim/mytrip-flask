2b1119c2ccb6b35cd330ce875e6df58e2639685b
kkj <rudwnd33@naver.com>
2021-10-13 14:23:28 +0900

    중복 함수 제거

--- static/js/indexScript.js
+++ static/js/indexScript.js
@@ -86,50 +86,6 @@ function sign_up() {
     });
 }
 
-function is_nickname(asValue) {
-    var regExp = /^(?=.*[a-zA-Z])[-a-zA-Z0-9_.]{2,10}$/;
-    return regExp.test(asValue);
-}
-
-function is_password(asValue) {
-    var regExp = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z!@#$%^&*]{8,20}$/;
-    return regExp.test(asValue);
-}
-
-function check_dup() {
-    let username = $("#input-username").val()
-    console.log(username)
-    if (username == "") {
-        $("#help-id").text("아이디를 입력해주세요.").removeClass("is-safe").addClass("is-danger")
-        $("#input-username").focus()
-        return;
-    }
-    if (!is_nickname(username)) {
-        $("#help-id").text("아이디의 형식을 확인해주세요. 영문과 숫자, 일부 특수문자(._-) 사용 가능. 2-10자 길이").removeClass("is-safe").addClass("is-danger")
-        $("#input-username").focus()
-        return;
-    }
-    $("#help-id").addClass("is-loading")
-    $.ajax({
-        type: "POST",
-        url: "/sign_up/check_dup",
-        data: {
-            username_give: username
-        },
-        success: function (response) {
-
-            if (response["exists"]) {
-                $("#help-id").text("이미 존재하는 아이디입니다.").removeClass("is-safe").addClass("is-danger")
-                $("#input-username").focus()
-            } else {
-                $("#help-id").text("사용할 수 있는 아이디입니다.").removeClass("is-danger").addClass("is-success")
-            }
-            $("#help-id").removeClass("is-loading")
-
-        }
-    });
-}
-
 function toggle_sign_up() {
     $("#sign-up-box").toggleClass("is-hidden")
     $("#div-sign-in-or-up").toggleClass("is-hidden")
@@ -140,12 +96,12 @@ function toggle_sign_up() {
 }
 
 function is_nickname(asValue) {
-    var regExp = /^(?=.*[a-zA-Z])[-a-zA-Z0-9_.]{8,14}$/;
+    let regExp = /^(?=.*[a-zA-Z])[-a-zA-Z0-9_.]{8,14}$/;
     return regExp.test(asValue);
 }
 
 function is_password(asValue) {
-    var regExp = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z!@#$%^&*]{8,20}$/;
+    let regExp = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z!@#$%^&*]{8,20}$/;
     return regExp.test(asValue);
 }
 
@@ -158,7 +114,7 @@ function check_dup() {
         return;
     }
     if (!is_nickname(username)) {
-        $("#help-id").text("아이디의 형식을 확인해주세요. 영문과 숫자, 일부 특수문자(._-) 사용 가능. 2-10자 길이").removeClass("is-safe").addClass("is-danger")
+        $("#help-id").text("아이디의 형식을 확인해주세요. 영문과 숫자, 일부 특수문자(._-) 사용 가능. 8-14자 길이").removeClass("is-safe").addClass("is-danger")
         $("#input-username").focus()
         return;
     }
