49cfc9e0fe96163d732d76b53a4e1f7cea74c11c
kkj <rudwnd33@naver.com>
2021-10-15 11:12:19 +0900

    app을 application으로 변경

--- application.py
+++ application.py
@@ -13,7 +13,7 @@ from datetime import datetime, timedelta
 # python-dotenv 라이브러리 설치
 from dotenv import load_dotenv
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 # .env 파일 만들어서 외부 노출 방지
 load_dotenv(verbose=True)
@@ -25,7 +25,6 @@ REQUEST_URL = os.getenv('REQUEST_URL')
 WEATHER_URL = os.getenv('WEATHER_URL')
 WEATHER_KEY = os.getenv('WEATHER_KEY')
 SECRET_KEY = os.getenv('SECRET_KEY')
-POPULAR_PLACE = os.getenv('POPULAR_PLACE')
 
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
@@ -37,13 +36,13 @@ db = client.myTrip
 
 
 # 로그인 페이지
-@app.route('/')
+@application.route('/')
 def login():
     msg = request.args.get("msg")
     return render_template('index.html', msg=msg)
 
 
-@app.route('/sign_in', methods=['POST'])
+@application.route('/sign_in', methods=['POST'])
 def sign_in():
     # 로그인
     username_receive = request.form['username_give']
@@ -65,7 +64,7 @@ def sign_in():
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
 
-@app.route('/sign_up/save', methods=['POST'])
+@application.route('/sign_up/save', methods=['POST'])
 def sign_up():
     username_receive = request.form['username_give']
     password_receive = request.form['password_give']
@@ -81,7 +80,7 @@ def sign_up():
     return jsonify({'result': 'success'})
 
 
-@app.route('/sign_up/check_dup', methods=['POST'])
+@application.route('/sign_up/check_dup', methods=['POST'])
 def check_dup():
     username_receive = request.form['username_give']
     exists = bool(db.users.find_one({"username": username_receive}))
@@ -89,7 +88,7 @@ def check_dup():
 
 
 # main.html 렌더링
-@app.route('/main', methods=['GET'])
+@application.route('/main', methods=['GET'])
 def main():
     token_receive = request.cookies.get('mytoken')
 
@@ -103,7 +102,7 @@ def main():
         return redirect(url_for("login", msg="login_error."))
 
 
-@app.route('/near', methods=['POST'])
+@application.route('/near', methods=['POST'])
 def get_near_place():
     token_receive = request.cookies.get('mytoken')
 
@@ -137,7 +136,7 @@ def get_near_place():
 
 
 # 랜덤으로 7가지 추천 테마 여행 띄어주기
-@app.route('/popular/list', methods=['POST'])
+@application.route('/popular/list', methods=['POST'])
 def get_popular_trips():
     info = random.randrange(1, 7)
     cat1 = 'C01'
@@ -187,7 +186,7 @@ def get_popular_trips():
 
 
 # nearList.html 렌더링
-@app.route('/near/list', methods=['GET'])
+@application.route('/near/list', methods=['GET'])
 def get_near_list():
     token_receive = request.cookies.get('mytoken')
 
@@ -202,7 +201,7 @@ def get_near_list():
 
 
 # nearList.html에 리스트 출력
-@app.route('/near/list', methods=['POST'])
+@application.route('/near/list', methods=['POST'])
 def get_near_type():
     lat_receive = request.form['lat_give']
     lng_receive = request.form['lng_give']
@@ -237,7 +236,7 @@ def get_near_type():
 
 
 # nearDetail.html 렌더링
-@app.route('/near/place/<content_id>', methods=['GET'])
+@application.route('/near/place/<content_id>', methods=['GET'])
 def get_near_detail(content_id):
     token_receive = request.cookies.get('mytoken')
 
@@ -252,7 +251,7 @@ def get_near_detail(content_id):
 
 
 # 즐겨찾기 기능 - 누가 어떤 여행지를 즐겨찾기 했는지 db에 저장
-@app.route("/near/place/bookmark", methods=['POST'])
+@application.route("/near/place/bookmark", methods=['POST'])
 def bookmark():
     token_receive = request.cookies.get('mytoken')
 
@@ -278,7 +277,7 @@ def bookmark():
 
 
 # 즐겨찾기한 게시글은 나갔다 들어와도 즐겨찾기로 표시
-@app.route('/near/place/bookmark/<content_id>', methods=['GET'])
+@application.route('/near/place/bookmark/<content_id>', methods=['GET'])
 def get_bookmark(content_id):
     token_receive = request.cookies.get('mytoken')
 
@@ -294,7 +293,7 @@ def get_bookmark(content_id):
 
 
 # 즐겨찾기 페이지에서 모아보기
-@app.route('/bookmark', methods=['GET'])
+@application.route('/bookmark', methods=['GET'])
 def show_bookmarks():
     token_receive = request.cookies.get('mytoken')
 
@@ -310,7 +309,7 @@ def show_bookmarks():
 
 
 # 즐겨찾기 된 아이디 전송
-@app.route('/bookmark', methods=['POST'])
+@application.route('/bookmark', methods=['POST'])
 def give_bookmarks_id():
     token_receive = request.cookies.get('mytoken')
 
@@ -329,7 +328,7 @@ def give_bookmarks_id():
 
 
 # 근처 여행지 날씨 불러오기
-@app.route('/near/place/weather', methods=['POST'])
+@application.route('/near/place/weather', methods=['POST'])
 def get_weather():
     place_lat = request.form['place_lat']
     place_lng = request.form['place_lng']
@@ -349,7 +348,7 @@ def get_weather():
 
 
 # 리뷰 정렬
-@app.route('/trips', methods=['GET'])
+@application.route('/trips', methods=['GET'])
 def show_trips():
     sort_type = request.args.get('sort')
 
@@ -365,7 +364,7 @@ def show_trips():
 
 
 # tripsList.html 렌더링
-@app.route('/trips/list', methods=['GET'])
+@application.route('/trips/list', methods=['GET'])
 def get_trips_list():
     token_receive = request.cookies.get('mytoken')
 
@@ -380,7 +379,7 @@ def get_trips_list():
 
 
 # tripsDetail.html 렌더링
-@app.route('/trips/place/<trip_id>', methods=['GET'])
+@application.route('/trips/place/<trip_id>', methods=['GET'])
 def get_trips_detail(trip_id):
     token_receive = request.cookies.get('mytoken')
 
@@ -398,7 +397,7 @@ def get_trips_detail(trip_id):
         return redirect(url_for("login", msg="login_error."))
 
 
-@app.route('/trips/place/render', methods=['POST'])
+@application.route('/trips/place/render', methods=['POST'])
 def trips_detail():
     trip_id_receive = request.form['trip_id_give']
 
@@ -410,7 +409,7 @@ def trips_detail():
 
 
 # 상황에 따라 write.html(작성폼), update.html(수정폼) 렌더링
-@app.route('/trips/form', methods=['GET'])
+@application.route('/trips/form', methods=['GET'])
 def write():
     token_receive = request.cookies.get('mytoken')
     trip_id = request.args.get('id')
@@ -436,7 +435,7 @@ def write():
 
 
 # 클라이언트에서 세션 스토리지에 저장하기 위함
-@app.route('/trips/session', methods=['POST'])
+@application.route('/trips/session', methods=['POST'])
 def update():
     trip_id_receive = request.form['trip_id_give']
 
@@ -451,7 +450,7 @@ def update():
 
 
 # 리뷰 db에 저장
-@app.route('/trips/place', methods=['POST'])
+@application.route('/trips/place', methods=['POST'])
 def write_trip():
     token_receive = request.cookies.get('mytoken')
     try:
@@ -506,7 +505,7 @@ def write_trip():
 
 
 # 리뷰 수정
-@app.route('/trips/place/<trip_id>', methods=['PUT'])
+@application.route('/trips/place/<trip_id>', methods=['PUT'])
 def update_trip(trip_id):
     trip_title_receive = request.form['title_give']
     trip_place_receive = request.form['place_give']
@@ -551,13 +550,13 @@ def update_trip(trip_id):
 
 
 # 리뷰 삭제
-@app.route('/trips/place/<trip_id>', methods=['DELETE'])
+@application.route('/trips/place/<trip_id>', methods=['DELETE'])
 def delete_trip(trip_id):
     db.trips.delete_one({'id': int(trip_id)})
     return jsonify({'msg': '삭제 완료!'})
 
 
-@app.route('/trips/place/like', methods=['POST'])
+@application.route('/trips/place/like', methods=['POST'])
 def like_place():
     trip_id_receive = request.form['trip_id_give']
 
@@ -571,7 +570,7 @@ def like_place():
     return jsonify({'msg': '좋아요 완료!'})
 
 
-@app.route('/profile', methods=['POST'])
+@application.route('/profile', methods=['POST'])
 def save_profile():
     token_receive = request.cookies.get('mytoken')
     try:
@@ -620,4 +619,5 @@ def save_profile():
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    application.debug = True
+    application.run()
