79494ae5bceaadbffe253cb69d54d8b22317f6ea
kkj <rudwnd33@naver.com>
2021-10-17 00:15:10 +0900

    리뷰 삭제 시 리뷰에 달린 댓글도 삭제

--- application.py
+++ application.py
@@ -825,7 +825,7 @@ def show_comments(trip_id):
 
         return jsonify({'all_comments': all_comments, 'now_user': payload['id']})
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-        return render_template('tripsDetail.html')
+        return redirect(url_for("login"))
 
 
 @application.route('/trips/place/comment/<trip_id>', methods=['DELETE'])
@@ -841,7 +841,7 @@ def delete_comment(trip_id):
 
         return jsonify({'result': 'success'})
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-        return render_template('tripsDetail.html')
+        return redirect(url_for("login"))
 
 
 if __name__ == '__main__':
